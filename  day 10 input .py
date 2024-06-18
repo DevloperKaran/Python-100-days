fc86becc768ca974e66fa221889d9de7d1cc7a03
DevloperKaran <dailydosebykaran77@gmail.com>
2024-06-18 10:42:04 +0530

    Python

---
+++  day 10 input .py
@@ -0,0 +1,19 @@
+
+# day 10 input
+
+a = input("Write your Name : ")
+
+print("Welcome ", a , "To PyCalculator")
+
+b= input("First Number :")
+
+c= input("Second Number :")
+
+print( " 1st Number + 2nd Number ","=",int(b) + int(c))
+print(" 1st Number - 2nd Number ","=",int(b) - int(c))
+print ("  1st Number × 2nd Number","=",int(b) * int(c))
+print(" 1st Number ÷ 2nd Number ","=", int(b)/int(c))
+print("1st Number % 2nd Number ", "=" , int(b)% int(c))
+print("1st Number // 2nd Number", "=",int(b)//int(c))
+print("%", "=", "Modules")
+print("//","=", 'Floor Division')
---
+++ Comments in Python.py
@@ -0,0 +1,8 @@
+
+
+# My name is karan this is single line comment
+
+''' my name is karan
+   Carry
+   Harry
+   this is mult line comments'''
